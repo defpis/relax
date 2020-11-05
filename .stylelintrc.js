0dce9600c194faace60a5d01d76816322f1fcb79
wenjun <wenjun.super@bytedance.com>
2020-11-05 23:34:26 +0800

    chore(stylelint): support lint style

---
+++ .stylelintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ['stylelint-order', 'stylelint-scss'],
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  rules: {
+    'comment-empty-line-before': null,
+    'declaration-empty-line-before': null,
+    'function-name-case': 'lower',
+    'no-descending-specificity': null,
+    'no-invalid-double-slash-comments': null,
+  },
+};
